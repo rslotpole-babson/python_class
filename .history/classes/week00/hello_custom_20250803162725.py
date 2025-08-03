024b1f789158c71b08bb39c3ba536dc0872909f6
rslotpole-babson <rslotpole@babson.edu>
2025-08-03 16:44:07 -0400

    create 5 files update hello_custom.py

---
+++ .history/classes/week00/hello_custom_20250803162725.py
@@ -0,0 +1,10 @@
+
+name = input('Please enter your name: ')
+print('Hello, ' + name)
+
+
+'''git remote add upstream https://github.com/rslotpole-babson/class-template.git
+git config --global pull.rebase false
+git config --global core.editor "code --wait"
+
+git pull --no-edit upstream main   '''
---
+++ .history/classes/week00/setup_20250803164338.py
@@ -0,0 +1,15 @@
+'''
+run the first 3 git commands only once
+
+git remote add upstream https://github.com/rslotpole-babson/class-template.git
+git config --global pull.rebase false
+git config --global core.editor "code --wait"
+
+each time you get the message from canvas that the template has been updated, 
+MAKE SURE YOU HAVE COMMITTED AND SYNCED YOUR CHANGES IN CODINGSPACES FIRST, and then run the following:
+
+git pull --no-edit upstream main 
+
+
+
+'''
