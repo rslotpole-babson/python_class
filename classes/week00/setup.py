cacd0a5ca12005938d678bf7140dfe11a595ff4b
rslotpole-babson <rslotpole@babson.edu>
2025-08-03 17:04:10 -0400

    update setup

--- classes/week00/setup.py
+++ classes/week00/setup.py
@@ -1,12 +1,20 @@
 '''
+step 1: log into your github account
+step 2: In the address bar go to: https://github.com/rslotpole-babson/python_class
+step 3: click on "Use this template" -> "create new repository"
+step 4: Make the repository name: "python-class-YOURNAME"
+step 5: click on "create repository"
+step 6: click on "Code" -> "Codespaces" -> "Create codespace on main"
+step 7:
+
 run the first 3 git commands only once
 
 git remote add upstream https://github.com/rslotpole-babson/class-template.git
 git config --global pull.rebase false
 git config --global core.editor "code --wait"
 
-each time you get the message from canvas that the template has been updated, 
-MAKE SURE YOU HAVE COMMITTED AND SYNCED YOUR CHANGES IN CODINGSPACES FIRST, and then run the following:
+*******each time you get the message from canvas that the template has been updated,                          *********
+*******MAKE SURE YOU HAVE COMMITTED AND SYNCED YOUR CHANGES IN CODINGSPACES FIRST, and then run the following:*********
 
 git pull --no-edit upstream main 
 
