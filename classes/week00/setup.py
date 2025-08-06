eead119359c62759b2e1e042f9f000a11499514c
rslotpole-babson <rslotpole@babson.edu>
2025-08-06 16:48:03 +0000

    build(deps): update setup.py

--- classes/week00/setup.py
+++ classes/week00/setup.py
@@ -9,18 +9,31 @@ step 7:
 
 run the first 3 git commands only once
 
-git remote add upstream https://github.com/rslotpole-babson/python_class.git
+git remote add upstream https://github.com/rslotpole-babson/python_class.git         
 git config --global pull.rebase false
 git config --global core.editor "code --wait"
 
-*******each time you get the message from canvas that the template has been updated,                          *********
-*******MAKE SURE YOU HAVE COMMITTED AND SYNCED YOUR CHANGES IN CODINGSPACES FIRST, and then run the following:*********
+***************************************************************************************************************
+*** each time you get the message from canvas that the template has been updated,                           ***
+*** MAKE SURE YOU HAVE COMMITTED AND SYNCED YOUR CHANGES IN CODINGSPACES FIRST, and then run the following: ***
+***************************************************************************************************************
 
 git pull --no-edit upstream main 
+
+*** IMPORTANT: NEVER DELETE YOUR REPO OR CODESPACE ***
+
+***************************************************
+*** The following commands SHOULD NOT be needed ***
+***************************************************
 
 command to merge unrelated histories (should not have to use)
 git pull --no-edit upstream main --allow-unrelated-histories
 
+command to rename branch
+git branch -m old-name new-name
+
+command to switch to main
+git checkout main
 
 
 '''
